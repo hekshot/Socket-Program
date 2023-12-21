110f5c18ded98349cfe2790eb1cd9faf02448415
hekshot <amankumarkr111@gmail.com>
2023-12-21 11:58:19 +0530

    encryption and decryption implemented

--- SimpleServer.java
+++ SimpleServer.java
@@ -1,9 +1,16 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
 
 public class SimpleServer {
     public static void main(String[] args) {
@@ -17,28 +24,29 @@ public class SimpleServer {
         ServerSocket serverSocket = null;
 
         try {
-            // Create a server socket
             serverSocket = new ServerSocket(portNumber);
             System.out.println("Server is listening on port " + portNumber);
 
-            // Wait for a client to connect
             Socket clientSocket = serverSocket.accept();
             System.out.println("Client connected: " + clientSocket.getInetAddress());
 
-            // Create input and output streams for communication
+            SecretKey secretKey = generateSecretKey();
+
             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
 
-            // Start a separate thread to handle messages from the client
-            new Thread(() -> handleClientMessages(in, out)).start();
+            out.println(Base64.getEncoder().encodeToString(secretKey.getEncoded()));
 
-            // Start a separate thread to handle messages from the server console
-            new Thread(() -> handleServerConsoleInput(out)).start();
+            Cipher rc4Cipher = Cipher.getInstance("RC4");
+            rc4Cipher.init(Cipher.DECRYPT_MODE, secretKey);
 
-        } catch (IOException e) {
+            new Thread(() -> handleClientMessages(in, out, rc4Cipher)).start();
+
+            new Thread(() -> handleServerConsoleInput(out, rc4Cipher)).start();
+
+        } catch (IOException | NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException e) {
             e.printStackTrace();
         } finally {
-            // Close the server socket in the finally block
             if (serverSocket != null && !serverSocket.isClosed()) {
                 try {
                     serverSocket.close();
@@ -49,34 +57,57 @@ public class SimpleServer {
         }
     }
 
-    private static void handleClientMessages(BufferedReader in, PrintWriter out) {
+    private static SecretKey generateSecretKey() throws NoSuchAlgorithmException {
+        KeyGenerator keyGen = KeyGenerator.getInstance("RC4");
+        return keyGen.generateKey();
+    }
+
+    private static String encryptMessage(String message, Cipher rc4Cipher) throws IOException {
+        try {
+            byte[] encryptedBytes = rc4Cipher.doFinal(message.getBytes());
+            return Base64.getEncoder().encodeToString(encryptedBytes);
+        } catch (IllegalBlockSizeException | BadPaddingException e) {
+            throw new IOException("Error encrypting message", e);
+        }
+    }
+
+    private static String decryptMessage(String encryptedMessage, Cipher rc4Cipher) throws IOException {
+        try {
+            byte[] decodedBytes = Base64.getDecoder().decode(encryptedMessage);
+            byte[] decryptedBytes = rc4Cipher.doFinal(decodedBytes);
+            return new String(decryptedBytes);
+        } catch (IllegalBlockSizeException | BadPaddingException e) {
+            throw new IOException("Error decrypting message", e);
+        }
+    }
+
+    private static void handleClientMessages(BufferedReader in, PrintWriter out, Cipher rc4Cipher) {
         try {
             String message;
             while ((message = in.readLine()) != null) {
-                System.out.println("Client message: " + message);
+                System.out.println("encrypted" + message);
+                System.out.println("Client message: " + decryptMessage(message, rc4Cipher));
 
                 if (message.equalsIgnoreCase("bye")) {
-                    out.println("Goodbye, client!");
+                    out.println(encryptMessage("Goodbye, client!", rc4Cipher));
                     System.out.println("Client disconnected.........");
                     break;
                 }
             }
         } catch (IOException e) {
-            // Handle client disconnect gracefully
             System.out.println("Client disconnected.........");
         }
     }
 
-    private static void handleServerConsoleInput(PrintWriter out) {
+    private static void handleServerConsoleInput(PrintWriter out, Cipher rc4Cipher) {
         try {
             BufferedReader serverInput = new BufferedReader(new InputStreamReader(System.in));
             String messageOut;
             while ((messageOut = serverInput.readLine()) != null) {
-                out.println(messageOut);
+                out.println(encryptMessage(messageOut, rc4Cipher));
 
-                // Break the loop if the user types "bye"
                 if (messageOut.equalsIgnoreCase("bye")) {
-                    out.println("Goodbye, server!");
+                    out.println(encryptMessage("Goodbye, server!", rc4Cipher));
                     break;
                 }
             }
